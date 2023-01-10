72a40235623386e7dc7fddb804519ee5fc3f575b
RDI <dkdlsekdls@naver.com>
2023-01-10 20:07:54 +0900

    update/useTabs

---
+++ useTabs/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
