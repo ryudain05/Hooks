7ce360c35cf1a2489f8d02af3259a015be320b3d
RDI <dkdlsekdls@naver.com>
2023-01-12 00:00:13 +0900

    feat

--- useTabs/index.js
+++ useTabs/index.js
@@ -5,12 +5,11 @@ const App = () => {
   const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      {content.map((section, index) => (
+      {content.map((section, index2) => (
         <button onClick={() => changeItem(index)}>{section.tab}</button>
       ))}
       <div>{currentItem.content}</div>
     </div>
-  );
 };
 
 const rootElement = document.getElementById("root");
